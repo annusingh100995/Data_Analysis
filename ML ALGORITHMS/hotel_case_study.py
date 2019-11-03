014c46428481f49329a3acdfc28bb429eb6fad9d
annusingh100995 <annusingh100995@gmail.com>
2019-11-03 12:40:41 +0000

    updated

--- ML ALGORITHMS/hotel_case_study.py
+++ ML ALGORITHMS/hotel_case_study.py
@@ -16,7 +16,7 @@ from collections import Counter
 from sklearn.linear_model import LinearRegression,LassoCV, Ridge, LassoLarsCV,ElasticNetCV
 from sklearn.model_selection import GridSearchCV, cross_val_score, learning_curve
 from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor, ExtraTreesRegressor, GradientBoostingRegressor
-from sklearn.preprocessing import StandardScaler, Normalizer, RobustScaler
+from sklearn.preprocessing import StandardScaler, Normalizer, RobustScaler, MinMaxScaler
 warnings.filterwarnings('ignore')
 sns.set(style='white', context='notebook', palette='deep')
 
@@ -282,5 +282,158 @@ for i, feature in zip(range(1,len(list)),list):
     plt.scatter(train[feature], train['n_clicks'], edgecolors='r')
     plt.xlabel(feature)
     plt.ylabel('n_clicks')
+    plt.title(feature)
+plt.show()
+
+# DATA CLEANING AND IMPUTING MISSING VALUES
+
+""" It is important to clean the data before analysing it. There can be missing values that can either be imputed or removed.
+    Depending upon the data we need to seelct the best way to clean the data.
+    We need to check wether the missing values are random or there is some pattern in the missing values.
+    We also don't want to remove all the missing data as it  might remove revelant information
+    Hence, this is a crucial step and need to be heed properly."""
+
+# number of rows in train and test data set
+ntrain = train.shape[0]
+ntest = test.shape[0]
+
+# number of clicks of the training dataset
+y_train = train.n_clicks.values
+
+# train and test data combined
+all_data = pd.concat((train, test)).reset_index(drop=True)
+# the n_clicks column removed
+all_data.drop(['n_clicks'], axis=1, inplace=True)
+
+print("Train data size is : {}".format(train.shape))
+print("Test data size is : {}".format(test.shape))
+print("Combined dataset size is : {}".format(all_data.shape))
+
+# Finding the missing data 
+
+all_data_na = (all_data.isnull().sum() / len(all_data)) * 100
+
+"""all_data_na
+avg_price              0.043129
+avg_rank               0.000000
+avg_rating            27.812027
+avg_saving_percent     0.043129
+city_id                0.125414
+content_score          0.125414
+distance_to_center     0.130900
+hotel_id               0.000000
+n_images               0.125792
+n_reviews              0.130900
+stars                  0.139412    
+
+    aug_price and hotel_id have zero missing values while the avg_rating is missing the most 27 % is missing """
+
+all_data_na = all_data_na.drop(all_data_na[all_data_na == 0].index).sort_values(ascending=False)[:30]
+missing_data = pd.DataFrame({'Missing Ratio' :all_data_na})
+missing_data
+
+""" missing_data
+                    Missing Ratio
+avg_rating              27.812027
+stars                    0.139412
+n_reviews                0.130900
+distance_to_center       0.130900
+n_images                 0.125792
+content_score            0.125414
+city_id                  0.125414
+avg_saving_percent       0.043129
+avg_price                0.043129   """
+
+
+# Plotting the statistics of missing data.
+
+f, ax = plt.subplots(figsize=(15, 10))
+plt.xticks(rotation='90')
+sns.barplot(x=all_data_na.index, y=all_data_na)
+plt.xlabel('Features', fontsize=10)
+plt.ylabel('Percent of missing values', fontsize=10)
+plt.title('Percent missing data by feature', fontsize=10)
+plt.show()
+
+train['avg_rating'].describe()
+"""
+count    286089.000000
+mean         81.616525
+std           6.025140
+min          34.000000
+25%          79.000000
+50%          83.000000
+75%          86.000000
+max          97.000000
+Name: avg_rating, dtype: float64    
+    75% of the avg_rating is below 86, mean of the average rating is ~82. I think using median to impute the avg_rating should be a good call
+    """"
+
+""" I'm imputing the features one by one , withouth using sklearn imputer. 
+    For avg_rating : 27% missing values, I'm imputing the missing values using the median
+    For stars, n_reviews, distance_to_center, n_images, content_score, avg_saving_percent the missing values are ~1% and I'm 
+        using mean values to replace the missing values
+    For city_id:  1.2% values are missing, I am using mode to impute the missing values. 
+        As I think the majority of the hotel to be in the major tourist attraction. So the mode gives the is id of the most
+        busy city. """
+
+# imptuing the data
+all_data['city_id'] = all_data['city_id'].fillna(all_data['city_id'].mode()[0])
+for col in ('n_reviews','avg_saving_percent','stars','content_score','avg_price','n_images','distance_to_center','avg_rank'):
+    all_data[col] = all_data[col].mode()[0]
+all_data['avg_rating'] = all_data['avg_rating'].fillna(all_data['avg_rating'].mean()[0])
+all_data["avg_rating"] = all_data["avg_rating"].transform(lambda x: x.fillna(x.median()))
+
+# Checking in missing data is there
+all_data_na = (all_data.isnull().sum() / len(all_data)) * 100
+all_data_na = all_data_na.drop(all_data_na[all_data_na == 0].index).sort_values(ascending=False)
+missing_data = pd.DataFrame({'Missing Ratio' :all_data_na})
+missing_data.head() 
+# no missing data
+
+# FEATURE TRANDFORAMTION
+""" The features are most of the time in different range. For example the avrage ratind and the distance.
+    hence for propr analyis, it is important to normalise the data to bring it into comparable scale."""
+
+names = train.columns
+# Create the Scaler object
+scaler = StandardScaler()
+# Fit your data on the scaler object
+scaled_df = scaler.fit_transform(train)
+scaled_df = pd.DataFrame(scaled_df, columns=names)
+
+list = ['n_reviews','avg_saving_percent','stars','content_score','avg_price','avg_rating','n_images','distance_to_center',
+    'city_id','avg_rank'] 
+   
+for i, feature in zip(range(1,len(list)),list):
+    plt.subplot(2,5,i)
+    sns.kdeplot(train[feature])
+    plt.title(feature)
+plt.show()
+
+for i, feature in zip(range(1,len(list)),list):
+    plt.subplot(2,5,i)
+    sns.kdeplot(scaled_df[feature])
+    plt.title(feature)
+plt.show()
+
+from sklearn.preprocessing import StandardScaler, Normalizer, RobustScaler, MinMaxScaler
+
+mm_scaler = MinMaxScaler()
+nor_scaler = Normalizer()
+rob_scaler = RobustScaler()
+
+mm_scaled_df = mm_scaler.fit_transform(train)
+mm_scaled_df = pd.DataFrame(mm_scaled_df, columns=names)
+
+nor_scaled_df = scaler.fit_transform(train)
+nor_scaled_df = pd.DataFrame(nor_scaled_df, columns=names)
+
+rob_scaled_df = scaler.fit_transform(train)
+rob_scaled_df = pd.DataFrame(rob_scaled_df, columns=names)
+
+for i, feature in zip(range(1,len(list)),list):
+    plt.subplot(2,5,i)
+    sns.kdeplot(rob_scaled_df[feature])
     plt.title(feature)
 plt.show()
